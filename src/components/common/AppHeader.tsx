1fb6bfb240d4103d3bc2fe2f5b6e773ab265b30b
suhrobKholmurodov <suhrobkholmurodov1@gmail.com>
2025-08-27 16:20:57 +0500

    style: improve UI components styling

--- src/components/common/AppHeader.tsx
+++ src/components/common/AppHeader.tsx
@@ -1,4 +1,10 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  LayoutPanelLeft,
+  PanelBottom,
+  PanelRight,
+} from "lucide-react";
 import vscode from "../../assets/vscode.png";
 
 const menuItems = [
@@ -14,7 +20,7 @@ const menuItems = [
 
 export function AppHeader() {
   return (
-    <header className="flex items-center h-8 w-full bg-zinc-900 border-b border-zinc-800 select-none">
+    <header className="flex items-center h-[35px] w-full bg-zinc-900 border-b border-zinc-800 select-none">
       <nav className="flex items-center pl-2 gap-2">
         <img src={vscode} alt="" width={"20px"} height={"20px"} />
         {menuItems.map((item) => (
@@ -26,21 +32,34 @@ export function AppHeader() {
           </span>
         ))}
       </nav>
-
       <div className="flex-1 flex justify-center items-center">
-        <span className="text-sm font-bold text-zinc-200 tracking-tight">
-          Suhrob Kholmurodov
-        </span>
-        <span className="ml-2 text-xs text-blue-400 font-medium">
-          Frontend Developer
-        </span>
+        <div className="flex items-center gap-2 pr-4">
+          <button className="w-7 h-7 flex items-center justify-center rounded hover:bg-zinc-800">
+            <ChevronLeft size={16} className="text-zinc-400" />
+          </button>
+          <button className="w-7 h-7 flex items-center justify-center rounded hover:bg-zinc-800">
+            <ChevronRight size={16} className="text-zinc-400" />
+          </button>
+        </div>
+        <div className="border border-zinc-700 bg-zinc-800 rounded-md px-20 py-[2px] flex gap-2 items-center">
+          <span className="text-sm text-zinc-200 tracking-tight">
+            Suhrob Kholmurodov
+          </span>
+          <span className="text-sm text-zinc-200 tracking-tight">|</span>
+          <span className="text-xs text-blue-400 font-medium">
+            Frontend Developer
+          </span>
+        </div>
       </div>
-      <div className="flex items-center gap-2 pr-4">
+      <div className="flex items-center pr-2">
         <button className="w-7 h-7 flex items-center justify-center rounded hover:bg-zinc-800">
-          <ChevronLeft size={16} className="text-zinc-400" />
+          <LayoutPanelLeft size={16} className="text-zinc-100" />
         </button>
         <button className="w-7 h-7 flex items-center justify-center rounded hover:bg-zinc-800">
-          <ChevronRight size={16} className="text-zinc-400" />
+          <PanelBottom size={16} className="text-zinc-100" />
+        </button>
+        <button className="w-7 h-7 flex items-center justify-center rounded hover:bg-zinc-800">
+          <PanelRight size={16} className="text-zinc-100" />
         </button>
       </div>
     </header>
--- src/components/common/FileTabs.tsx
+++ src/components/common/FileTabs.tsx
@@ -20,30 +20,38 @@ export function FileTabs({
   onCloseTab: (id: string) => void;
 }) {
   return (
-    <div className="bg-zinc-900 border-b flex items-center h-8 overflow-x-auto">
+    <div className="bg-zinc-900 border-b flex items-center h-auto">
       {openFiles.map((file) => (
         <div
           key={file.id}
           className={cn(
-            "flex items-center px-3 py-1 gap-2 cursor-pointer text-xs border-r border-zinc-800",
+            "flex items-center px-3 py-[10px] gap-2 cursor-pointer text-xs border-r border-zinc-700 relative group",
             activeFileId === file.id
               ? "bg-zinc-800 text-yellow-200"
               : "bg-zinc-900 text-zinc-400 hover:bg-zinc-800"
           )}
           onClick={() => setActiveFileId(file.id)}
         >
+          {activeFileId === file.id && (
+            <div className="absolute top-0 left-0 right-0 h-0.5 bg-blue-500" />
+          )}
+
           <span className="font-semibold">{file.label}</span>
           <span className="opacity-70">{file.path.split("/")[0]}</span>
+
           <Button
             size="icon"
             variant="ghost"
-            className="ml-2"
+            className={cn(
+              "ml-2 h-[20px] w-[20px] rounded-sm hover:bg-zinc-700 hover:text-gray-300 opacity-0 group-hover:opacity-100 transition-opacity",
+              activeFileId === file.id && "opacity-100 hover:bg-zinc-600 text-white hover:text-white"
+            )}
             onClick={(e) => {
               e.stopPropagation();
               onCloseTab(file.id);
             }}
           >
-            <X size={14} />
+            <X size={10} />
           </Button>
         </div>
       ))}
