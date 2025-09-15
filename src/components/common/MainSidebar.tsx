bc0bfe5933d781e05ecf4c64bfc4081bb91d5e96
suhrobKholmurodov <suhrobkholmurodov1@gmail.com>
2025-09-15 10:56:18 +0500

    feat: added tooltip to the main sidebar

--- src/components/common/MainSidebar.tsx
+++ src/components/common/MainSidebar.tsx
@@ -1,6 +1,11 @@
 import { Github, Folders, Settings, CircleUserRound } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useNavigate } from "react-router-dom";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const sidebarIcons = [
   { id: "explorer", icon: <Folders size={24} />, label: "Explorer" },
@@ -35,35 +40,40 @@ export function MainSidebar({
     <nav className="bg-zinc-900 border-r border-zinc-500 flex flex-col justify-between h-full items-center">
       <div className="flex flex-col items-center">
         {sidebarIcons.map(({ id, icon, label }) => (
-          <button
-            key={id}
-            aria-label={label}
-            onClick={() => handleClick(id)}
-            className={cn(
-              "mb-2 w-[45px] h-[50px] flex items-center justify-center rounded transition-colors outline-none border-none relative",
-              activeSection === id
-                ? "bg-zinc-900 text-gray-100"
-                : "bg-zinc-900 text-zinc-500 hover:text-white"
-            )}
-            tabIndex={0}
-          >
-            {activeSection === id && (
-              <span className="absolute left-0 top-0 h-full w-[2px] bg-blue-500" />
-            )}
-            <span className="z-10 sm:text-lg">{icon}</span>
-          </button>
+          <Tooltip key={id}>
+            <TooltipTrigger asChild>
+              <button
+                aria-label={label}
+                onClick={() => handleClick(id)}
+                className={cn(
+                  "mb-2 w-[45px] h-[50px] flex items-center justify-center rounded transition-colors outline-none border-none relative",
+                  activeSection === id
+                    ? "bg-zinc-900 text-gray-100"
+                    : "bg-zinc-900 text-zinc-500 hover:text-white"
+                )}
+                tabIndex={0}
+              >
+                {activeSection === id && (
+                  <span className="absolute left-0 top-0 h-full w-[2px] bg-blue-500" />
+                )}
+                <span className="z-10 sm:text-lg">{icon}</span>
+              </button>
+            </TooltipTrigger>
+            <TooltipContent side="right">{label}</TooltipContent>
+          </Tooltip>
         ))}
       </div>
 
-      <div className=" flex flex-col items-center">
+      <div className="flex flex-col items-center">
         {bottomIcons.map(({ id, icon, label }) => (
-          <div
-            key={id}
-            aria-label={label}
-            className="w-[45px] h-[50px] flex items-center justify-center rounded text-zinc-500 cursor-default relative"
-          >
-            <span className="z-10 sm:text-lg">{icon}</span>
-          </div>
+          <Tooltip key={id}>
+            <TooltipTrigger asChild>
+              <div className="w-[45px] h-[50px] flex items-center justify-center rounded text-zinc-500 cursor-pointer relative">
+                <span className="z-10 sm:text-lg">{icon}</span>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent side="right">{label}</TooltipContent>
+          </Tooltip>
         ))}
       </div>
     </nav>
